e72a432dc01e309b682bce0ecf9f7caa3a2a3017
Infinite_Loop() <mike.computerwiz@gmail.com>
2017-03-07 14:48:15 -0500

    Initial Commit
    Initial Commit

---
+++ assets/js/carousel.js
@@ -0,0 +1,11 @@
+/**
+ * Created by mikericcardi on 2/17/17.
+ */
+// When the DOM is ready, run this function
+$(document).ready(function() {
+    //Set the carousel options
+    $('#quote-carousel').carousel({
+        pause: true,
+        interval: 10000,
+    });
+});
